9ee808d98a0ccd47a9c0d5cd788ac38496e7fd9d
Liel Fridman <lielft@gmail.com>
2020-09-07 18:47:16 +0300

    Basic rating component

---
+++ components/ui/Rating.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="rating-bar flex">
+    <font-awesome-icon
+      v-for="n in rating"
+      :key="n + '_full'"
+      :icon="['fas', 'star']"
+    />
+    <font-awesome-icon
+      v-for="n in 5 - rating"
+      :key="n + '_empty'"
+      :icon="['far', 'star']"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Rating',
+  props: {
+    rating: {
+      type: Number,
+      default: 0,
+    },
+    editable: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
