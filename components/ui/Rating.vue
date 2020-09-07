cf4494c9bc45c6158efac14b7a413ccfa4a578c8
Liel Fridman <lielft@gmail.com>
2020-09-07 19:17:03 +0300

    Rating component done

--- components/ui/Rating.vue
+++ components/ui/Rating.vue
@@ -1,14 +1,13 @@
 <template>
-  <div class="rating-bar flex">
+  <div class="rating-bar flex" :class="{ 'cursor-pointer': editable }">
     <font-awesome-icon
-      v-for="n in rating"
-      :key="n + '_full'"
-      :icon="['fas', 'star']"
-    />
-    <font-awesome-icon
-      v-for="n in 5 - rating"
-      :key="n + '_empty'"
-      :icon="['far', 'star']"
+      v-for="n in 5"
+      :key="n"
+      class="fa-2x text-primary"
+      :icon="[displayRating >= n ? 'fas' : 'far', 'star']"
+      @mouseover="mouseOver(n)"
+      @mouseout="mouseOut"
+      @click="ratingSet(n)"
     />
   </div>
 </template>
@@ -20,10 +19,38 @@ export default {
     rating: {
       type: Number,
       default: 0,
+      validator(value) {
+        return value >= 0 && value <= 5
+      },
     },
     editable: {
       type: Boolean,
       default: false,
+    },
+  },
+  data() {
+    return {
+      hoverRating: null,
+    }
+  },
+  computed: {
+    displayRating() {
+      return this.hoverRating === null ? this.rating : this.hoverRating
+    },
+  },
+  methods: {
+    ratingSet(number) {
+      this.$emit('rating-set', number)
+    },
+    mouseOver(n) {
+      if (this.editable) {
+        this.hoverRating = n
+      }
+    },
+    mouseOut() {
+      if (this.editable) {
+        this.hoverRating = null
+      }
     },
   },
 }
