e1082b03b3778f7a149bb7ca8ea9002882f2f57d
Liel Fridman <lielft@gmail.com>
2020-09-08 14:05:20 +0300

    Adding semantic elements to the default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <top-bar></top-bar>
-    <Nuxt class="px-6 py-2 flex flex-col items-center min-w-full" />
+    <header>
+      <top-bar></top-bar>
+    </header>
+    <main>
+      <Nuxt class="px-6 py-2 flex flex-col items-center min-w-full" />
+    </main>
   </div>
 </template>
