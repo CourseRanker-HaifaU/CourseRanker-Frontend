db17bb242f1d040f58061ee34f8c574090116f15
Shir Avneri <shiravneri@gmail.com>
2020-09-08 16:59:21 +0300

    Login page edited, no inner border and more

--- pages/login.vue
+++ pages/login.vue
@@ -1,9 +1,12 @@
 <template>
-  <div class="flex h-cover w-full">
-    <div
-      class="w-full bg-white rounded-lg shadow-lg p-8 m-4 md:max-w-sm text-center"
-    >
-      <h1 class="login-header text-grey-darkest text-4xl mb-6">כניסה לאתר</h1>
+  <div class="flex h-cover w-full text-center">
+    <div class="w-full p-8 m-4 md:max-w-sm">
+      <img
+        class="cs-img m-auto rounded-full"
+        src="~/assets/images/logoranker.png"
+        alt="לוגו ועד הסטודנטים"
+      />
+      <h1 class="login-header text-4xl mb-6">כניסה לאתר</h1>
       <form>
         <div class="mb-4">
           <label>
@@ -12,7 +15,7 @@
               type="email"
               name="email"
               placeholder="אימייל"
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
+              class="shadow-md appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
             />
           </label>
         </div>
@@ -23,7 +26,7 @@
               type="password"
               name="password"
               placeholder="סיסמה"
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              class="shadow-md appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             />
           </label>
         </div>
@@ -49,15 +52,24 @@ export default {
 </script>
 
 <style>
-input[type='email']:focus,
-input[type='password']:focus {
-  outline: none;
-  box-shadow: 0 0 0 2pt #d51111;
+.cs-img {
+  border: 3px solid #a6a8a9;
 }
-
 .login-header {
   margin-bottom: 30px;
   color: #0664aa;
+}
+
+input[type='email'],
+input[type='password'] {
+  border: 1px solid #a6a8a9;
+}
+
+input[type='email']:focus,
+input[type='password']:focus {
+  outline: none;
+  border: none;
+  box-shadow: 0 0 0 2pt #d51111;
 }
 .submit-btn {
   background-color: #0664aa;
