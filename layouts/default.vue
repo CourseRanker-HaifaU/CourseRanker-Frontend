8fd50f8361c95b76b756bf311125d619b420bc88
Liel Fridman <lielft@gmail.com>
2020-09-08 14:04:01 +0300

    Main container is full width

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
     <top-bar></top-bar>
-    <Nuxt class="px-6 py-2 flex flex-col items-center self-center" />
+    <Nuxt class="px-6 py-2 flex flex-col items-center min-w-full" />
   </div>
 </template>
