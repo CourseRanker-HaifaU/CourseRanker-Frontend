ab046d462e2048988bf0d98a7c23b2c38afae09f
sagigvili <57597612+sagigvili@users.noreply.github.com>
2020-09-09 23:11:50 +0300

    Courses Table is now a compenent

---
+++ components/layout/CoursesTable.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="frame-div flex items-stretch">
+    <table>
+      <tr class="catagories_row bg-primary text-white">
+        <th class="border px-8 py-4">שם הקורס</th>
+        <th class="border px-8 py-4">סימסטר</th>
+        <th class="border px-8 py-4">נקודות זכות</th>
+        <th class="border px-8 py-4">סוג קורס</th>
+      </tr>
+      <tr class="items_row cursor-pointer">
+        <td class="border px-8 py-4">מבוא למדעי המחשב</td>
+        <td class="border px-8 py-4">א'</td>
+        <td class="border px-8 py-4">5</td>
+        <td class="border px-8 py-4">חובה</td>
+      </tr>
+      <tr class="items_row cursor-pointer">
+        <td class="border px-8 py-4">מתמטיקה דיסקרטית</td>
+        <td class="border px-8 py-4">א'</td>
+        <td class="border px-8 py-4">5</td>
+        <td class="border px-8 py-4">חובה</td>
+      </tr>
+      <tr class="items_row cursor-pointer">
+        <td class="border px-8 py-4">חדו"א א</td>
+        <td class="border px-8 py-4">א'</td>
+        <td class="border px-8 py-4">5</td>
+        <td class="border px-8 py-4">חובה</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<style>
+.catagories_row {
+  text-align: right;
+}
+.items_row:hover {
+  background-color: #cacad3;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,32 +4,7 @@
     <search-bar class="label__search"></search-bar>
     <br />
     <nuxt-link to="/course">מבוא למדעי המחשב</nuxt-link>
-    <table>
-      <tr class="catagories_row">
-        <th class="border px-8 py-4">שם הקורס</th>
-        <th class="border px-8 py-4">סימסטר</th>
-        <th class="border px-8 py-4">נקודות זכות</th>
-        <th class="border px-8 py-4">סוג קורס</th>
-      </tr>
-      <tr class="items_row">
-        <td class="border px-8 py-4">מבוא למדעי המחשב</td>
-        <td class="border px-8 py-4">א'</td>
-        <td class="border px-8 py-4">5</td>
-        <td class="border px-8 py-4">חובה</td>
-      </tr>
-      <tr class="items_row">
-        <td class="border px-8 py-4">מתמטיקה דיסקרטית</td>
-        <td class="border px-8 py-4">א'</td>
-        <td class="border px-8 py-4">5</td>
-        <td class="border px-8 py-4">חובה</td>
-      </tr>
-      <tr class="items_row">
-        <td class="border px-8 py-4">חדו"א א</td>
-        <td class="border px-8 py-4">א'</td>
-        <td class="border px-8 py-4">5</td>
-        <td class="border px-8 py-4">חובה</td>
-      </tr>
-    </table>
+    <courses-table></courses-table>
   </div>
 </template>
 
@@ -48,7 +23,6 @@ export default {
 <style>
 .catagories_row {
   text-align: right;
-  background-color: mediumturquoise;
 }
 .items_row:hover {
   background-color: #cacad3;
