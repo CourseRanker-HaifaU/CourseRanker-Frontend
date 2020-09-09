28774c453a7be012114be642c56411cb57a175af
sagigvili <57597612+sagigvili@users.noreply.github.com>
2020-09-09 22:57:31 +0300

    Initial Courses table

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,32 @@
     <search-bar class="label__search"></search-bar>
     <br />
     <nuxt-link to="/course">מבוא למדעי המחשב</nuxt-link>
+    <table>
+      <tr class="catagories_row">
+        <th class="border px-8 py-4">שם הקורס</th>
+        <th class="border px-8 py-4">סימסטר</th>
+        <th class="border px-8 py-4">נקודות זכות</th>
+        <th class="border px-8 py-4">סוג קורס</th>
+      </tr>
+      <tr class="items_row">
+        <td class="border px-8 py-4">מבוא למדעי המחשב</td>
+        <td class="border px-8 py-4">א'</td>
+        <td class="border px-8 py-4">5</td>
+        <td class="border px-8 py-4">חובה</td>
+      </tr>
+      <tr class="items_row">
+        <td class="border px-8 py-4">מתמטיקה דיסקרטית</td>
+        <td class="border px-8 py-4">א'</td>
+        <td class="border px-8 py-4">5</td>
+        <td class="border px-8 py-4">חובה</td>
+      </tr>
+      <tr class="items_row">
+        <td class="border px-8 py-4">חדו"א א</td>
+        <td class="border px-8 py-4">א'</td>
+        <td class="border px-8 py-4">5</td>
+        <td class="border px-8 py-4">חובה</td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -20,6 +46,14 @@ export default {
 </script>
 
 <style>
+.catagories_row {
+  text-align: right;
+  background-color: mediumturquoise;
+}
+.items_row:hover {
+  background-color: #cacad3;
+}
+
 /* Sample `apply` at-rules with Tailwind CSS
 .container {
 @apply min-h-screen flex justify-center items-center text-center mx-auto;
