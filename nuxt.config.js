a627405fc1e25a8191af0c54832e0ba2a226f448
Liel Fridman <lielft@gmail.com>
2020-09-12 19:51:42 +0300

    Change the page title to something meaningful

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
     htmlAttrs: {
       dir: 'rtl',
     },
-    title: process.env.npm_package_name || '',
+    title: 'משוב קורסים',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
