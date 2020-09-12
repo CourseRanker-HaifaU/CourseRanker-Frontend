d637563d97f934d5c01f22a1d52996ae31fbfb4f
sagigvili <57597612+sagigvili@users.noreply.github.com>
2020-09-12 11:54:25 +0300

    Mod. and fixes

--- components/layout/CoursePage.vue
+++ components/layout/CoursePage.vue
@@ -1,41 +1,42 @@
 <template>
   <div class="flex flex-col gap-y-10">
     <div class="container">
-      <div class="divide-y-2">
-        <h2>מבוא למדעי המחשב</h2>
-        <div class="grid grid-flow-col grid-cols-4 grid-rows-2 gap-4">
-          <div>
-            <th>חוג:</th>
-            <td>מדעי המחשב</td>
-          </div>
-          <div>
-            <th>נקודות זכות:</th>
-            <td>5</td>
-          </div>
-          <div>
-            <th>אופן הוראה:</th>
-            <td>שיעור ותרגול</td>
-          </div>
-          <div>
-            <th>קהל יעד:</th>
-            <td>חוג</td>
-          </div>
-          <div>
-            <th>סוג קורס:</th>
-            <td>חובה</td>
-          </div>
-          <div>
-            <th>דירוג ממוצע:</th>
-            <td>
-              <rating :rating="5" style="font-size: 7px"></rating>
-            </td>
-          </div>
+      <h2>מבוא למדעי המחשב</h2>
+      <div style="margin-bottom: 18px">
+        <hr class="border-0 bg-gray-500 text-gray-500 h-px" />
+      </div>
+      <div class="grid grid-flow-col grid-cols-4 grid-rows-2 gap-2">
+        <div>
+          <th>חוג:</th>
+          <td>מדעי המחשב</td>
+        </div>
+        <div>
+          <th>נקודות זכות:</th>
+          <td>5</td>
+        </div>
+        <div>
+          <th>אופן הוראה:</th>
+          <td>שיעור ותרגול</td>
+        </div>
+        <div>
+          <th>קהל יעד:</th>
+          <td>חוג</td>
+        </div>
+        <div>
+          <th>סוג קורס:</th>
+          <td>חובה</td>
+        </div>
+        <div>
+          <th>דירוג ממוצע:</th>
+          <td>
+            <rating :rating="5" class="rating"></rating>
+          </td>
         </div>
       </div>
     </div>
     <div class="container">
       <h1>סימסטר א' תשפ"א</h1>
-      <div class="grid gap-1 grid-cols-2 gap-y-5">
+      <div class="grid gap-1 grid-cols-2 gap-y-3">
         <div>
           <th>מרצה:</th>
           <td>אור מאיר</td>
@@ -47,31 +48,31 @@
         <div>
           <th>דירוג מרצה:</th>
           <td>
-            <rating :rating="5" style="font-size: 7px"></rating>
+            <rating :rating="5" class="rating"></rating>
           </td>
         </div>
         <div>
           <th>דירוג מתרגל/ת:</th>
           <td>
-            <rating :rating="5" style="font-size: 7px"></rating>
+            <rating :rating="5" class="rating"></rating>
           </td>
         </div>
         <div>
           <th>דירוג קורס:</th>
           <td>
-            <rating :rating="5" style="font-size: 7px"></rating>
+            <rating :rating="5" class="rating"></rating>
           </td>
         </div>
         <div>
           <th>דירוג קורס + מרצה:</th>
           <td>
-            <rating :rating="5" style="font-size: 7px"></rating>
+            <rating :rating="5" class="rating"></rating>
           </td>
         </div>
         <div>
           <th>דירוג קורס + מתרגל/ת:</th>
           <td>
-            <rating :rating="5" style="font-size: 7px"></rating>
+            <rating :rating="5" class="rating"></rating>
           </td>
         </div>
       </div>
@@ -86,11 +87,15 @@ h2 {
   @apply ml-6;
 }
 h1 {
+  padding-bottom: 0.75rem;
   @apply text-xl;
   @apply font-bold;
   @apply ml-2;
 }
 td {
   padding-right: 0.5rem;
+}
+.rating {
+  font-size: 6px;
 }
 </style>
