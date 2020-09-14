5b2d2ef8fcea7e743728f5e0f76d8a77ac5939d2
Liel Fridman <lielft@gmail.com>
2020-09-14 22:37:24 +0300

    CoursesTable tweaks and an alternative card layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,9 +4,9 @@
       <top-bar></top-bar>
     </header>
     <main
-      class="flex flex-col items-center md:mx-32 xl:mx-64 bg-white px-6 py-2 flex-grow shadow-2xl"
+      class="flex flex-col items-center lg:mx-32 xl:mx-64 bg-white px-3 md:px-6 py-2 flex-grow shadow-2xl"
     >
-      <Nuxt class="flex flex-col items-center lg:w-full lg:min-w-full" />
+      <Nuxt class="flex flex-col items-center w-full min-w-full" />
     </main>
   </div>
 </template>
