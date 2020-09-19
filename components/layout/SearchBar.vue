b453b331726e8e15395d24ca10bafd6114ecac3b
Liel Fridman <lielft@gmail.com>
2020-09-20 00:16:43 +0300

    Main admin page tweaks

--- components/layout/SearchBar.vue
+++ components/layout/SearchBar.vue
@@ -57,7 +57,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .overall-div {
   @apply flex;
   @apply self-center;
--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,56 +1,49 @@
 <template>
   <div>
-    <div class="flex flex-col gap-y-10 mt-4 min-w-full items-stretch">
-      <h1 class="text-3xl font-bold ml-6">פאנל מנהל</h1>
-      <div style="margin-bottom: 18px">
-        <hr class="border-0 bg-gray-500 text-gray-500 h-px" />
-      </div>
+    <div class="my-4 min-w-full">
+      <h1 class="text-4xl text-primary">פאנל מנהל</h1>
+      <hr class="border-0 bg-gray-500 text-gray-500 h-px" />
     </div>
-    <div>
-      <button class="button blue-button ml-2">הוספת קורס חדש</button>
-      <button class="button blue-button ml-2">הוספת סימסטר לקורס</button>
-      <button class="button blue-button ml-2">דיווחי תוכן פוגעני</button>
+    <div class="self-start">
+      <button class="button blue-button ml-2 my-2">הוספת קורס חדש</button>
+      <button class="button blue-button ml-2 my-2">הוספת סימסטר לקורס</button>
+      <button class="button blue-button ml-2 my-2">דיווחי תוכן פוגעני</button>
     </div>
-    <div class="h-cover w-full text-center">
-      <h1 style="margin-left: 1200px; margin-top: 40px">
-        <b>
-          <u>הגדרת סימסטר נוכחי</u>
-        </b>
-      </h1>
-      <div class="w-full p-4 m-2 md:max-w-md">
-        <form class="emulated-flex-gap" @submit.prevent="onSubmit">
-          <input-field
-            id="hebrew"
-            v-model="hebYear"
-            type="hebrew"
-            label="שנה עברית"
-            div-class="mb-8 text-right"
-          />
-          <input-field
-            id="foreign"
-            v-model="foriegnYear"
-            type="foreign"
-            label="שנה לועזית"
-            div-class="mb-8 text-right"
-          />
-          <div class="flex flex-col">
-            <div style="margin-left: 20px; margin-bottom: 5px">סימסטר:</div>
-            <select v-model="selected" :class="divClass">
-              <option>א'</option>
-              <option>ב'</option>
-              <option>קיץ</option>
-            </select>
-          </div>
-          <div class="flex flex-col">
-            <input
-              id="addSemester"
-              type="submit"
-              value="הוסף"
-              class="button red-button mt-2"
-            />
-          </div>
-        </form>
-      </div>
+    <div class="h-cover w-full mt-6">
+      <h2 class="text-2xl underline">הגדרת סמסטר נוכחי</h2>
+      <form
+        class="flex flex-col md:flex-row min-w-full items-stretch justify-around py-4 md:my-2 md:max-w-md"
+        @submit.prevent="onSubmit"
+      >
+        <input-field
+          id="hebrew"
+          v-model="hebYear"
+          type="text"
+          label="שנה עברית"
+          div-class="text-right mx-2"
+        />
+        <input-field
+          id="foreign"
+          v-model="foriegnYear"
+          type="number"
+          label="שנה לועזית"
+          div-class="text-right mx-2"
+        />
+        <div class="mx-2">
+          <label for="selectedSemester">סמסטר:</label>
+          <select id="selectedSemester" v-model="selected" class="form-field">
+            <option>א'</option>
+            <option>ב'</option>
+            <option>קיץ</option>
+          </select>
+        </div>
+        <input
+          id="addSemester"
+          type="submit"
+          value="הוסף"
+          class="button red-button mt-6 mx-2"
+        />
+      </form>
     </div>
   </div>
 </template>
@@ -58,20 +51,3 @@
 <script>
 export default {}
 </script>
-
-<style scoped>
-h1 {
-  margin-right: 1rem;
-}
-
-.emulated-flex-gap > * {
-  margin: 18px 0 0 18px;
-}
-
-.emulated-flex-gap {
-  display: inline-flex;
-  flex-wrap: nowrap;
-  margin: -18px 0 0 -18px;
-  width: calc(100% + 12px);
-}
-</style>
