51834b4a80150a1509dfbd24d5ef8efca7b6bc3d
sagigvili <57597612+sagigvili@users.noreply.github.com>
2020-09-19 19:18:46 +0300

    Updated semester define

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -11,19 +11,45 @@
       <button class="button blue-button ml-2">הוספת סימסטר לקורס</button>
       <button class="button blue-button ml-2">דיווחי תוכן פוגעני</button>
     </div>
-    <div>
-      <h1 style="margin-left: 1200px; margin-top: 70px">
-        <b><u>הגדרת סימסטר נוכחי</u></b>
+    <div class="h-cover w-full text-center">
+      <h1 style="margin-left: 1200px; margin-top: 40px">
+        <b>
+          <u>הגדרת סימסטר נוכחי</u>
+        </b>
       </h1>
-      <p style="margin-right: 30px">
-        שנה עברית:
-        <input />
-        שנה לועזית:
-        <input />
-        סימסטר:
-        <input />
-        <button class="button red-button ml-2">הוסף</button>
-      </p>
+      <div class="w-full p-4 m-2 md:max-w-md">
+        <form class="emulated-flex-gap" @submit.prevent="onSubmit">
+          <input-field
+            id="hebrew"
+            v-model="hebYear"
+            type="hebrew"
+            label="שנה עברית"
+            div-class="mb-8 text-right"
+          />
+          <input-field
+            id="foreign"
+            v-model="foriegnYear"
+            type="foreign"
+            label="שנה לועזית"
+            div-class="mb-8 text-right"
+          />
+          <input-field
+            id="semester"
+            v-model="semester"
+            type="semester"
+            label="סימסטר"
+            div-class="mb-8 text-right"
+          />
+          <div class="flex flex-col">
+            <input
+              id="addSemester"
+              type="submit"
+              value="הוסף"
+              class="button red-button mt-2"
+            />
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 </template>
@@ -37,9 +63,14 @@ h1 {
   margin-right: 1rem;
 }
 
-input:focus {
-  outline: none;
-  border: 0;
-  box-shadow: none;
+.emulated-flex-gap > * {
+  margin: 18px 0 0 18px;
+}
+
+.emulated-flex-gap {
+  display: inline-flex;
+  flex-wrap: nowrap;
+  margin: -18px 0 0 -18px;
+  width: calc(100% + 12px);
 }
 </style>
