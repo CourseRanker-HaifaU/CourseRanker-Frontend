30afeff0270a1fd60c1e72a9442ed452172ac8b4
Liel Fridman <lielft@gmail.com>
2020-10-13 15:42:10 +0300

    Fixing the modal and editing the grades

--- pages/admin/grades_manager.vue
+++ pages/admin/grades_manager.vue
@@ -66,91 +66,10 @@
                 <td class="row">
                   <button
                     class="table-btn min-w-full xxl:min-w-0"
-                    @click="popModal()"
+                    @click="popModal(index)"
                   >
                     הצג התפלגות
                   </button>
-                  <!--Modal-->
-                  <div
-                    v-if="modalOn"
-                    class="modal opacity-0 pointer-events-none fixed w-full h-full top-0 left-0 flex items-center justify-center"
-                  >
-                    <div
-                      class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"
-                    ></div>
-
-                    <div
-                      class="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto"
-                    >
-                      <div
-                        class="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50"
-                      >
-                        <svg
-                          class="fill-current text-white"
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="18"
-                          height="18"
-                          viewBox="0 0 18 18"
-                        >
-                          <path
-                            d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"
-                          ></path>
-                        </svg>
-                        <span class="text-sm">(Esc)</span>
-                      </div>
-
-                      <!-- Add margin if you want to see some of the overlay behind the modal-->
-                      <div class="modal-content py-4 text-left px-6">
-                        <!--Title-->
-                        <div class="flex justify-between items-center pb-3">
-                          <p class="text-2xl font-bold">התפלגות הציונים</p>
-                          <div class="modal-close cursor-pointer z-50">
-                            <svg
-                              class="fill-current text-black"
-                              xmlns="http://www.w3.org/2000/svg"
-                              width="18"
-                              height="18"
-                              viewBox="0 0 18 18"
-                            >
-                              <path
-                                d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"
-                              ></path>
-                            </svg>
-                          </div>
-                        </div>
-
-                        <!--Body-->
-                        <div
-                          v-for="i in 10"
-                          :key="`histogram_${index}_bin_${i}`"
-                          class="flex flex-col items-right rounded-lg border-input-border border border-solid mb-2 p-2 text-right"
-                          dir="ltr"
-                        >
-                          <label :for="`histogram_${index}_bin_${i}_input`">
-                            {{ histogramMapping[i - 1] }}:
-                          </label>
-                          <input-field
-                            :id="`histogram_${index}_bin_${i}_input`"
-                            v-model.number="
-                              rows.edges[index].node.histogram[i - 1]
-                            "
-                            type="number"
-                            :label="histogramMapping[i - 1]"
-                          ></input-field>
-                        </div>
-
-                        <!--Footer-->
-                        <div class="flex justify-end pt-2">
-                          <button
-                            class="modal-close px-4 bg-indigo-500 p-3 rounded-lg text-white hover:bg-indigo-400"
-                            @click="popModal()"
-                          >
-                            סגור
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
                 </td>
                 <td class="row">
                   <span
@@ -209,32 +128,85 @@
               </tr>
             </tbody>
           </table>
+          <!--Modal-->
           <div
-            class="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between"
+            v-if="modalOn"
+            class="modal fixed w-full h-full top-0 left-0 flex items-center justify-center"
           >
-            <div class="inline-flex mt-2 xs:mt-0">
-              <button
-                v-if="page != 1"
-                class="ml-2 button blue-button"
-                @click="page--"
+            <div
+              class="modal-overlay fixed w-full h-full bg-gray-900 opacity-50"
+              @click="popModal"
+            ></div>
+
+            <div
+              class="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg overflow-y-auto z-50"
+            >
+              <div
+                class="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50"
+                @click="popModal"
               >
-                קודם
-              </button>
-              <button
-                v-for="pageNumber in pages.slice(page - 1, page + 5)"
-                :key="pageNumber"
-                class="ml-2 button blue-button"
-                @click="page = pageNumber"
-              >
-                {{ pageNumber }}
-              </button>
-              <button
-                v-if="page < pages.length"
-                class="rounded-r button blue-button"
-                @click="page++"
-              >
-                הבא
-              </button>
+                <svg
+                  class="fill-current text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="18"
+                  height="18"
+                  viewBox="0 0 18 18"
+                >
+                  <path
+                    d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"
+                  ></path>
+                </svg>
+              </div>
+
+              <!-- Add margin if you want to see some of the overlay behind the modal-->
+              <div class="modal-content py-4 text-left px-6">
+                <!--Title-->
+                <div class="flex justify-between items-center pb-3">
+                  <p class="text-2xl font-bold">התפלגות הציונים</p>
+                  <div class="modal-close cursor-pointer z-50">
+                    <svg
+                      class="fill-current text-black"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="18"
+                      height="18"
+                      viewBox="0 0 18 18"
+                      @click="popModal"
+                    >
+                      <path
+                        d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"
+                      ></path>
+                    </svg>
+                  </div>
+                </div>
+
+                <!--Body-->
+                <div
+                  v-for="i in 10"
+                  :key="`histogram_bin_${i}`"
+                  class="flex flex-col items-right rounded-lg border-input-border border border-solid mb-2 p-2 text-right"
+                  dir="ltr"
+                >
+                  <label :for="`histogram_bin_${i}_input`">
+                    {{ histogramMapping[i - 1] }}:
+                  </label>
+                  <input-field
+                    :id="`histogram_bin_${i}_input`"
+                    v-model.number="histogram[i - 1]"
+                    type="number"
+                    :label="histogramMapping[i - 1]"
+                  ></input-field>
+                </div>
+
+                <!--Footer-->
+                <div class="flex justify-end pt-2">
+                  <button
+                    class="modal-close px-4 bg-indigo-500 p-3 rounded-lg text-white hover:bg-indigo-400"
+                    @click="popModal"
+                  >
+                    סגור
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -285,6 +257,8 @@ export default {
         '91+',
       ],
       modalOn: false,
+      modalIndex: -1,
+      histogram: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     }
   },
   computed: {
@@ -314,10 +288,13 @@ export default {
   },
   methods: {
     getSemester,
-    popModal() {
+    popModal(index) {
       if (this.modalOn) {
+        this.rows.edges[this.modalIndex].node.histogram = [...this.histogram]
         this.modalOn = false
       } else {
+        this.histogram = [...this.rows.edges[index].node.histogram]
+        this.modalIndex = index
         this.modalOn = true
       }
     },
@@ -361,6 +338,7 @@ export default {
             id: this.rows.edges[id].node.id,
             moed: this.rows.edges[id].node.moed,
             average: this.rows.edges[id].node.average,
+            histogram: this.rows.edges[id].node.histogram,
           },
         },
       })
