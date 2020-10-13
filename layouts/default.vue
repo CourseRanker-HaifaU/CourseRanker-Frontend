d5a9e4e066fc3b75df9e4109d0457b43accfdb34
Liel Fridman <lielft@gmail.com>
2020-10-13 23:45:41 +0300

    Fixing height problems on some browsers

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="flex flex-col md:h-screen">
+  <div class="flex-1 flex flex-col">
     <header>
       <top-bar></top-bar>
     </header>
     <main
-      class="flex flex-col items-center lg:mx-32 xl:mx-64 bg-white px-3 md:px-6 py-2 flex-grow shadow-2xl"
+      class="flex-1 flex flex-col items-center lg:mx-32 xl:mx-64 bg-white px-3 md:px-6 py-2 flex-grow shadow-2xl"
     >
-      <Nuxt class="flex flex-col items-center w-full min-w-full h-full" />
+      <Nuxt
+        class="flex-1 flex flex-col items-center w-full min-w-full h-full"
+      />
     </main>
   </div>
 </template>
@@ -31,9 +33,3 @@ export default {
   },
 }
 </script>
-
-<style scoped>
-.almost-full-height {
-  height: 100vh;
-}
-</style>
